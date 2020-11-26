97ee5e84556a662bc6323dcfceea5d1ec40ace2a
bingotop <wodnjs3600@naver.com>
2020-11-26 22:52:16 +0900

    11/26

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import "./styles/style.css";
 // declare
 const limit = '&limit=20';
 const apiKey = "2Od9A5B5VJRzJhtCeLaKsIQLuuACsiix";
-const Url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`;
+const Url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=`;
 const basicRenderingUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
 const main = document.querySelector(".search-result");
 const form = document.querySelector(".search-form");
-const searchInput = document.getElementById("search");
+const searchInput = document.querySelector("input");
+const container = document.querySelector(".popup-container");
+const popupCloseBtn = document.getElementById("close");
+const popUp = document.querySelector(".popup");
 //https://api.giphy.com/v1/gifs/random?api_key=2Od9A5B5VJRzJhtCeLaKsIQLuuACsiix
 
 //function
@@ -26,7 +29,7 @@ const firstRender = async()=>{
 }
 
 const createImages = (gifs)=>{
-    if(gifs !== null){
+    if(gifs !== (null || undefined)){
     gifs.map((gif)=>{
         let img = document.createElement("img");
         img.src = gif.images.fixed_height.url;
@@ -40,7 +43,7 @@ const getGifs = async(event)=>{
     event.preventDefault();
     beforeRender();
     const searchInputValue = searchInput.value;
-    const gifyUrl = `${Url}${searchInputValue}${limit}`;
+    const gifyUrl = `${Url}${searchInputValue}`;
     const response = await fetch(gifyUrl);
     const data = await response.json();
     const gifData = await data.data;
@@ -48,8 +51,59 @@ const getGifs = async(event)=>{
     searchInput.value = "";  
 }
 
+
 // eventlistener
 form.addEventListener("submit",getGifs);
+main.addEventListener("click",(e)=>{
+    if(e.target.classList.value === "results-gif"){
+        const imgUrl = e.target.src;
+        let popupImg = document.createElement("img");
+        let span = document.createElement("span");
+        let popupInput = document.createElement("input");
+        let icon = document.createElement("f");
+        const iconList = ["far","fa-clone","copy"];
+        iconList.forEach((iconClass)=>{
+            icon.classList.add(iconClass)
+        });
+        popupInput.value = imgUrl;
+        popupImg.src = imgUrl;
+        popupImg.classList.add("results-gif");
+        popupInput.classList.add("copy-input");
+        span.appendChild(popupInput);
+        span.appendChild(icon);
+        popUp.appendChild(popupImg);
+        popUp.appendChild(span);
+        container.classList.add("active");
+    }    
+});
+popupCloseBtn.addEventListener("click",()=>{
+    container.classList.remove("active");
+    const gifImg = popUp.querySelector(".results-gif");
+    const copytags = popUp.querySelector("span")
+    popUp.removeChild(gifImg);
+    popUp.removeChild(copytags);
+});
+
+popUp.addEventListener("click",(e)=>{
+    if(e.target.tagName === "F"){
+        const copyInput = document.querySelector(".copy-input");
+        const icon = document.querySelector("f.fa-clone");
+        let tempElem = document.createElement("textarea");
+        const copyComplete = document.querySelector(".complete");
+        //Copy
+        document.body.appendChild(tempElem);
+        tempElem.value = copyInput.value;
+        tempElem.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempElem);
+        copyComplete.classList.toggle("hide");
+
+        
+        // setTimeout(copyBtn.addEventListener("click",()=>{
+        //     copyBtn.classList.remove("active");
+        // }),3000);
+    }
+})
 
 // function call
 firstRender();
